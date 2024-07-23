2af3bdcfa61eff47ceb6e7e5459152536d1cc38b
Mohammed Tariq <mooode208@gmail.com>
2024-07-23 14:31:16 +0300

    Update Controller.java

--- src/main/java/com/example/exerciseintroductiontospringboot/Controller.java
+++ src/main/java/com/example/exerciseintroductiontospringboot/Controller.java
@@ -1,7 +1,6 @@
 package com.example.exerciseintroductiontospringboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
